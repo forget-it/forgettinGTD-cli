90ab96902225e530a28d5f8bf5856e19dc8289f3
Stefan Schnuderl <github@schnuderl.co>
2015-01-07 18:03:22 +0100

    Create t2100-help.sh

---
+++ tests/t2100-help.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+
+test_description='help functionality
+This test covers the help output.
+'
+. ./actions-test-lib.sh
+. ./test-lib.sh
+
+# Note: To avoid having to adapt the test whenever the help documentation
+# slightly changes, only check for the section headers.
+test_todo_session 'help output' <<EOF
+>>> todo.sh help | sed '/^  [A-Z]/!d'
+  Usage: todo.sh [-fhpantvV] [-d todo_config] action [task_number] [task_description]
+  Options:
+  Built-in Actions:
+EOF
+
+test_done
